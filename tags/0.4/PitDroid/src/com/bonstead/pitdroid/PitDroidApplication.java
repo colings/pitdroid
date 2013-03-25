bd5606e92d29abb307940adafc5f992c3ef77f5b
colin@bonstead.com <colin@bonstead.com@7b375f84-8b15-415a-7a99-e0786be53834>
2013-03-25 16:17:29 +0000

    Oops

--- tags/0.4/PitDroid/src/com/bonstead/pitdroid/PitDroidApplication.java
+++
@@ -1,8 +0,0 @@
-package com.bonstead.pitdroid;
-
-import android.app.Application;
-
-public class PitDroidApplication extends Application
-{
-    public HeaterMeter mHeaterMeter = new HeaterMeter();
-}
