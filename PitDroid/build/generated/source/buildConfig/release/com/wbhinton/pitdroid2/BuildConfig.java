0cebf411af63bf3cf6d954ae88f18396999e8eba
wbhinton <wbhinton@gmail.com>
2015-09-11 14:21:31 -0500

    Updates for Compatibility
    I converted the project to Android Studio, and updated the the app to be
    compatible with Nexus 6 and Android M

---
+++ PitDroid/build/generated/source/buildConfig/release/com/wbhinton/pitdroid2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wbhinton.pitdroid2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.bonstead.pitdroid";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "";
+}
