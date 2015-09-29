50414d35484b0904452b3e9458198eaadd5e730f
colings <github@bonstead.com>
2015-09-29 08:29:29 -0500

    Graph fixes
    - Now the legend shows again.
    - Changed the legend to use the default height. (maybe DPI aware?  Might
    fix tiny fonts.)

--- app/src/main/java/com/bonstead/pitdroid/GraphActivity.java
+++ app/src/main/java/com/bonstead/pitdroid/GraphActivity.java
@@ -24,6 +24,8 @@ import com.androidplot.ui.YLayoutStyle;
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYLegendWidget;
 import com.androidplot.xy.XYPlot;
 
 import com.bonstead.pitdroid.HeaterMeter.NamedSample;
@@ -112,33 +114,35 @@ public class GraphActivity extends Fragment implements HeaterMeter.Listener,
 		mPlot.getLayoutManager().remove(mPlot.getDomainLabelWidget());
 		mPlot.getLayoutManager().remove(mPlot.getRangeLabelWidget());
 
-		// Adjust the legend so it stretches across the entire screen, since we've got a
-		// lot of text to fit there
-		mPlot.getLegendWidget().setSize(
-				new SizeMetrics(20, SizeLayoutType.ABSOLUTE, 0, SizeLayoutType.FILL));
-		mPlot.getLegendWidget().position(0, XLayoutStyle.ABSOLUTE_FROM_LEFT, 0,
-				YLayoutStyle.ABSOLUTE_FROM_BOTTOM, AnchorPosition.LEFT_BOTTOM);
-
 		// Turn off the borders
 		mPlot.setBorderStyle(BorderStyle.NONE, null, null);
 
 		// Max out the size of the graph widget, so it fills the screen
-		mPlot.getGraphWidget().setSize(
+		XYGraphWidget graphWidget = mPlot.getGraphWidget();
+		graphWidget.setSize(
 				new SizeMetrics(0, SizeLayoutType.FILL, 0, SizeLayoutType.FILL));
-		mPlot.getGraphWidget().position(0, XLayoutStyle.ABSOLUTE_FROM_LEFT, 0,
+		graphWidget.position(0, XLayoutStyle.ABSOLUTE_FROM_LEFT, 0,
 				YLayoutStyle.ABSOLUTE_FROM_TOP);
+
+		// Adjust the legend so it stretches across the entire screen, since we've got a
+		// lot of text to fit there
+		XYLegendWidget legend = mPlot.getLegendWidget();
+		float defaultHeight = legend.getHeightMetric().getValue();
+		legend.setSize(new SizeMetrics(defaultHeight, SizeLayoutType.ABSOLUTE, 0, SizeLayoutType.FILL));
+		legend.position(0, XLayoutStyle.ABSOLUTE_FROM_LEFT, 0,
+				YLayoutStyle.ABSOLUTE_FROM_BOTTOM, AnchorPosition.LEFT_BOTTOM);
 
 		// Set all the background colors to the same value
 		mPlot.getBackgroundPaint().setColor(kGraphBackground);
-		mPlot.getGraphWidget().getBackgroundPaint().setColor(kGraphBackground);
-		mPlot.getGraphWidget().getGridBackgroundPaint().setColor(kGraphBackground);
+		graphWidget.getBackgroundPaint().setColor(kGraphBackground);
+		graphWidget.getGridBackgroundPaint().setColor(kGraphBackground);
 
 		// Boost up the top margin a bit, so the text for the highest value doesn't get
 		// cut off
-		mPlot.getGraphWidget().setMarginTop(10);
-		mPlot.getGraphWidget().setMarginRight(10);
+		graphWidget.setMarginTop(10);
+		graphWidget.setMarginRight(10);
 		// Add some extra room on the bottom, so the legend doesn't overlap
-		mPlot.getGraphWidget().setMarginBottom(20);
+		graphWidget.setMarginBottom(20);
 
 		// Force the range (temperature) to always be from 0-1, since we normalize them
 		// so we can display fan speed on the same graph.
